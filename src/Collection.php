c1409c8beaa103c943a8a521d02739fd02e17e65
Wallace de Souza <wallacemaxters@gmail.com>
2016-07-03 17:55:09 -0300

    adding new method filterByPrefix

--- src/Collection.php
+++ src/Collection.php
@@ -11,108 +11,130 @@ use PHPLegends\Collections\ListCollection;
  * */
 class Collection extends ListCollection
 {
-	/**
-	 * @var array
-	 * */
-	protected $items = [];
+    /**
+     * 
+     * 
+     * @param \PHPLegends\Routes\Route $route
+     * @return self
+    */
+    public function attach(Route $route)
+    {
+        $this->add($route);
 
-	/**
-	* @param \WallaceMaxters\SevenFramework\Routing\Route $route
-	* @return \WallaceMaxters\SevenFramework\Routing
-	*/
-	public function attach(Route $route)
-	{
-		$this->add($route);
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Adds route in collection 
+     * 
+     * @param Route $route
+     * @throws |UnexpectedValueException if non Route instance passed
+     * */
+    public function add($route)
+    {
+        if (! $route instanceof Route) {
 
-	/**
-	 * @param Route $route
-	 * @throws |UnexpectedValueException if non Route instance passed
-	 * 
-	 * */
-	public function add($route)
-	{
-		if (! $route instanceof Route)
-		{
-			throw new \UnexpectedValueException('Only Route can be added');
-		}
+            throw new \UnexpectedValueException('Only Route can be added');
+        }
 
-		return parent::add($route);
-	}
+        return parent::add($route);
+    }
 
-	/**
-	 * 
-	 * @param string|array $verb
-	 * @return \PHPlegends\Route\Route | null
-	*/
-	public function findByVerb($verb)
-	{
-		return $this->first($this->getVerbFilter($verb));
-	}
+    /**
+     * 
+     * @param string|array $verb
+     * @return \PHPlegends\Route\Route | null
+    */
+    public function findByVerb($verb)
+    {
+        return $this->first($this->getVerbFilter($verb));
+    }
 
-	/**
-	 * 
-	 * @param string $pattern
-	 * @return \PHPlegends\Route\Route | null
-	*/
-	public function findByUri($pattern)
-	{
-		return $this->first($this->getUriFilter($pattern));
-	}
+    /**
+     * 
+     * @param string $pattern
+     * @return \PHPlegends\Route\Route | null
+    */
+    public function findByUri($pattern)
+    {
+        return $this->first($this->getUriFilter($pattern));
+    }
 
-	/**
-	 * 
-	 * @param string $uri
-	 * @return \PHPLegends\Routes\Router
-	 * */
-	public function filterByUri($uri)
-	{
-		return $this->filter($this->getUriFilter($uri));
-	}
+    /**
+     * 
+     * @param string $uri
+     * @return \PHPLegends\Routes\Router
+     * */
+    public function filterByUri($uri)
+    {
+        return $this->filter($this->getUriFilter($uri));
+    }
 
-	/**
-	 * 
-	 * @param string $pattern
-	 * @return \PHPLegends\Routes\Router
-	 * */
-	public function filterByVerb($verb)
-	{
-		return $this->filter($this->getVerbFilter($verb));
-	}
+    /**
+     * 
+     * @param string $pattern
+     * @return \PHPLegends\Routes\Router
+     * */
+    public function filterByVerb($verb)
+    {
+        return $this->filter($this->getVerbFilter($verb));
+    }
 
-	protected function getVerbFilter($verb)
-	{
-		return function ($route) use ($verb) {
+    /**
+     * 
+     * @param string $verb
+     * @return \Closure
+     * */
+    protected function getVerbFilter($verb)
+    {
+        return function (Route $route) use ($verb) {
 
-			return $route->acceptedVerb($verb);
+            return $route->acceptedVerb($verb);
+        };
+    }
 
-		};
-	}
+    /**
+     * 
+     * 
+     * @param string $uri
+     * @return \Closure
+     * */
+    protected function getUriFilter($uri)
+    {
+        return function (Route $route) use ($uri) {
 
-	protected function getUriFilter($uri)
-	{
-		return function ($route) use ($uri) {
+            return $route->isValid($uri);
+        };
+    }
 
-			return $route->isValid($uri);
-		};
-	}
-
-	/**
-	 * 
-	 * @param callable $callback
-	 * 
-	 * @return Listcollection
-	 * */
-	public function map(callable $callback = null)
-	{	
-		$items = array_map(
+    /**
+     * 
+     * @param callable $callback
+     * @return Listcollection
+     * */
+    public function map(callable $callback = null)
+    {   
+        $items = array_map(
             $callback,
             $this->all(),
             $keys = $this->keys()
         );
 
         return new ListCollection(array_combine($keys, $items));
-	}
+    }
+
+    /**
+     * Filter by prefix name
+     *  
+     * @param string $name
+     * @return \PHPLegends\Routes\Collection
+     * */
+    public function filterByPrefixName($name)
+    {
+        return $this->filter(function (Route $route) use ($name)
+        {
+            return strpos($route->getName(), $name) === 0;
+        });
+    }
+
 }
