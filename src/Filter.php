3354ba56e90ca5fb05514637dfd251d0a5929dc0
Wallace de Souza <wallacemaxters@gmail.com>
2016-07-03 17:52:47 -0300

    Removing unecessary methods

--- src/Filter.php
+++ src/Filter.php
@@ -10,87 +10,69 @@ namespace PHPLegends\Routes;
 
 class Filter
 {
-	/**
-	 * 
-	 * @var callable
-	 * */
-	protected $callback;
+    /**
+     * 
+     * @var callable
+     * */
+    protected $callback;
 
-	/**
-	 * 
-	 * @var string
-	 * */
-	protected $name;
+    /**
+     * 
+     * @var string
+     * */
+    protected $name;
 
-	/**
-	 * 
-	 * 
-	 * @param string $name
-	 * @param callable $callback
-	 * */
-	public function __construct($name, callable $callback)
-	{
-		$this->name = $name;
+    /**
+     * 
+     * 
+     * @param string $name
+     * @param callable $callback
+     * */
+    public function __construct($name, callable $callback)
+    {
+        $this->name = $name;
 
-		$this->callback = $callback;
-	}
+        $this->callback = $callback;
+    }
 
-	/**
-	 * 
-	 * @return callable
-	 * */
-	public function getCallback()
-	{
-		return $this->callback;
-	}
+    /**
+     * 
+     * @return callable
+     * */
+    public function getCallback()
+    {
+        return $this->callback;
+    }
 
-	/**
-	 * Gets the name value
-	 * 
-	 * @return string
-	 * */
-	public function getName()
-	{
-		return $this->name;
-	}
+    /**
+     * Gets the name value
+     * 
+     * @return string
+     * */
+    public function getName()
+    {
+        return $this->name;
+    }
 
-	/**
-	 * Invoke the callable of filter
-	 * 
-	 * @param mixed[] ...$args
-	 * @return mixed
-	 * */
-	public function __invoke()
-	{
-		return call_user_func_array($this->getCallback(), func_get_args());
-	}
+    /**
+     * Invoke the callable of filter
+     * 
+     * @param ... $args
+     * @return mixed
+     * */
+    public function invoke()
+    {
+        return call_user_func_array($this->getCallback(), func_get_args());
+    }
 
-	public function raiseExceptionIfNotNull()
-	{
-		$result = call_user_func_array($this, func_get_args());
-
-		if ($result === null) return;
-
-		if ($result instanceof \Exception)
-		{
-			throw $result;
-
-		} elseif ($this->shouldBeException($result)) {	
-
-			throw new \Exception((string) $result);
-		}
-
-		return $result;
-
-	}
-
-	protected function shouldBeException($e)
-	{
-		return is_string($e) 
-			|| (is_object($e) && method_exists($e, '__toString'));
-
-	}
-
-
-
+    /**
+     * Invoke the callable of filter
+     * 
+     * @param ... $args
+     * @return mixed
+     * */
+    public function __invoke()
+    {
+        return call_user_func_array($this->getCallback(), func_get_args());
+    }
 }
