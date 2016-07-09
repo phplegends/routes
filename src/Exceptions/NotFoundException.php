cb63b57bbe4dbea54c7b123518649727917064fa
Wallace de Souza <wallacemaxters@gmail.com>
2016-07-09 19:03:39 -0300

    remove unecessary methods in exceptions

--- src/Exceptions/NotFoundException.php
+++ src/Exceptions/NotFoundException.php
@@ -5,15 +5,4 @@ namespace PHPLegends\Routes\Exceptions;
 /**
  * @author Wallace de Souza Vizerra <wallacemaxters@gmail.com>
  * */
-class NotFoundException extends RouteException
-{
-	/**
-	 * @param string $message
-	 * @return void
-	 * 
-	 * */
-	public function __construct($message)
-	{
-		parent::__construct($message, 404);
-	}
-}
+class NotFoundException extends RouteException {}
